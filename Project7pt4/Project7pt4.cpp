2bd961de6df138827ded5ef3f4010a7be3570559
mitchellmar <mitchell_k_mar@yahoo.com>
2021-03-21 17:53:01 -0700

    edit

--- Project7pt4/Project7pt4.cpp
+++ Project7pt4/Project7pt4.cpp
@@ -17,13 +17,29 @@ int main() {
 bool anyPrime(const std::vector<int>& values) {
     // TODO: return true if any of the values are prime numbers
     bool result = false;
-    std::list<int> list= { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
-    std::vector<int> primes(list.begin(), list.end());
-    std::vector<int> v(values.size() + primes.size());
-    std::vector<int>::iterator it;
-    it = std::set_intersection(primes.begin(), primes.end(), values.begin(), values.end(), v.begin());
-    if (v.front() != 0) {
-        result = true;
+
+    for (auto itr : values) {
+
+        if (itr == 1) {
+            continue;
+        }
+
+        bool isPrime = true;   
+
+        for (int i = 2; i <= sqrt(itr); i++) { 
+
+            if (itr % i == 0) {
+
+                isPrime = false;
+                break; 
+            }
+        }
+
+        result = isPrime;
+
+        if (isPrime) {
+            break;
+        }
     }
     return result;
 }
